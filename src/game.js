1fd95a74c3f86598cf61fd6e76035848296dcd91
David Pohan <0ronon0@gmail.com>
2016-12-10 17:38:44 +0100

    Fix missing position

--- src/game.js
+++ src/game.js
@@ -8,7 +8,7 @@
         state.ctx.fillStyle = '#ca0aca';
         state.ctx.translate(250, 150);
         state.ctx.rotate(2.54);
-        state.ctx.fillText('Dayum, a game!', , 0);
+        state.ctx.fillText('Dayum, a game!', 0, 0);
 
         return state;
     }
